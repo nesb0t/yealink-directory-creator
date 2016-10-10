d272ed1e30190b0d27ae3dd23a3796cf619396a1
Brent Nesbit <BNesbit@celito.net>
2016-10-09 21:02:34 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,118 @@
+# XML Directory Creator for Yealink & Netsapiens
+This simple script was made to automatically generate XML directories for Yealink phones that are connected to the Netsapiens platform. It will automatically create a directory that includes the full name and extension for each user in a domain. It will output a different directory for each domain.
+
+# Motivation
+While Netsapiens does have a directory creator, it doesn't work how I needed it to with Yealink phones. Users on our previous phone system were used to having a directory on the phone that listed all internal users, and we wanted to provide that same functionality on Netsapiens.
+
+This type of feature was posted as a feature request on the Netsapiens forum ([#545](https://forum.netsapiens.com/t/link-contacts-in-web-portal-to-show-on-yealink-directory/545/)) so I decided to release this for all to use. **I have added a TON of comments to the code, and broken it up in to several sections to make it easy to read**. Even those with very little PHP experience should be able to modify this to work for you.
+
+# Important Note Before Using
+The #1 thing that is likely to cause people trouble is found on line #109:
+```php
+if(isset($value->srv_code) && $value->srv_code == 'active-user') {
+```
+This line is doing two things to see if a user should be added to the directory. First it checks if there is any value in the srv_code field, and then it checks if that value is set to "active-user". If either of those are false, then the user will not be added. There are likely very few people here that won't have to change that in some way. If you don't change it, you will end up with completely blank directories. 
+
+I have it setup this way because our internal billing tool tracks billable users with the "active-user" service code, and we don't want non-billable users to show up in the directory. If you use a different service code for this type of thing then you can simply change "active-user" to whatever code you want to match on (which is why I left it in). If you do not use service codes at all then you must remove this line **and** the associated closing curly brace. If you don't remove both, you will have errors and the script won't run. Here is a before and after example to help if you must remove them.
+
+**BEFORE:**
+```php
+109     if(isset($value->srv_code) && $value->srv_code == 'active-user'){
+110         if($value->dir_list == 'yes'){	
+111             $userList .= '<DirectoryEntry>'."\n";
+112             $userList .= '<Name>' . $value->first_name . " " . $value->last_name ."</Name> \n";
+113             $userList .= '<Telephone>' . $value->user . "</Telephone> \n";
+114             $userList .= '</DirectoryEntry>'."\n";
+115         }
+116     }
+```
+**AFTER:**
+```php
+109     
+110         if($value->dir_list == 'yes'){	
+111             $userList .= '<DirectoryEntry>'."\n";
+112             $userList .= '<Name>' . $value->first_name . " " . $value->last_name ."</Name> \n";
+113             $userList .= '<Telephone>' . $value->user . "</Telephone> \n";
+114             $userList .= '</DirectoryEntry>'."\n";
+115         
+116     }
+```
+# Installation and Usage
+1. Read the section above prior to contining, or you will probably have blank directories.
+2. Open yealink-directory-creator.php in your favorite text editor (don't use Notepad) and make the necessary changes to the items found in the header. The actual file contains comments next to each item to help you out.
+```php
+define("SERVER", "nms.example.com");
+define("SUPERUSER", "directorycreator@example.com");
+define("PASSWORD", "Strong-Password-Here");
+define("CLIENTID", "Example_API_User");
+define("CLIENTSECRET", "ExampleKey123");
+define("DIRECTORYLOCATION", "/var/www/html/example/");
+```
+3. Place yealink-directory-creator.php in a non-web-accessible folder on any server that has PHP available.
+4. Configure a cron job (Linux) or Scheduled Task (Windows) to run yealink-directory-creator.php automatically every hour (or however often you want).
+```sh
+0 * * * * php -q -f /home/example/protected/directory-creator/yealink-directory-creator.php > /dev/null 2>&1
+```
+- The directories will be output to the folder specified in DIRECTORYLOCATION. You will want this to be web accessible. Ensure the user running the cron job has permissions to write to that folder. 
+- See the **Security** and **Troubleshooting** sections below for more tips and help.
+
+# Deploying to Yealink Phones
+Once the directories are created, you must deploy them to the phones using the remote directory feature. We do it in the NDP via domain overrides. If Netsapiens accepts feature request [#425](https://forum.netsapiens.com/t/ndp-adding-tokens-variables-to-overrides/425) then this will be even easier. If you want to thank me for this script, please go vote for it. :)
+
+```php
+remote_phonebook.data.1.url="http://example.com/directory/Example.com.xml"
+remote_phonebook.data.1.name="Example"
+features.remote_phonebook.enable="1"
+features.remote_phonebook.flash_time="3600"
+directory_setting.url="http://example.com/directory/favorite_setting.xml"
+```
+
+Review Yealink documentation if you have any questions about this, and feel free to contact me if you're still stuck. With additional overrides you configure it so that it goes directly to this phonebook and disable the local phonebook entirely, as well as a few more things.
+
+# Troubleshooting
+- There are a number of items that start are commented out by the pound sign (#) rather than my usual //. These items will also have a trailing comment that reads "*// Uncomment for basic debugging*." If you are having trouble getting this to work, you can uncomment those lines as necessary to figure out where it may be failing. Just run the file from the command line or make it web-accessible in your local environment and access it from your web browser.
+- You may contact me if you have any additional questions.
+
+# Tests
+- PHP: Tested on PHP version 5.6.23. I have not tested on v7, but it should be fine.
+- OS: Windows and Linux (Debian and Ubuntu).
+- Phones: Tested on Yealink T23G, T41P, T42G, T46G, and T48G. Any model that supports their XML directories should be fine.
+
+# Security
+- Do not store the PHP script itself in a web-accessible folder. That is a huge risk to your API keys and passwords.
+- Setup htaccess on the folder that the directories are written to (DIRECTORYLOCATION) and limit access to them. Some (non-foolproof) suggestions are to lock it down to certain IP addresses and/or to Yealink User-Agents. You can also obfuscate the file/folder names if you're concerned about someone scanning for them.
+- The account that is used to access the API should be a Read Only Super User rather than just Super User.
+
+# Contributors and Thanks
+- I used Chris Aaker's doCurl function from [here](https://github.com/aaker/domain-selfsignup). Thanks, Chris!
+
+# Disclaimer
+I am not a developer by any means. I barely knew PHP when I started this. I can't promise that this won't cause any problems for you, up to and including your web server catching on fire. Use it at your own risk. You should test it on a sandbox before you use it on your production servers.
+
+With that said, we have been using it for over a year without any issues. The API calls are all read-only, and you should be using a read-only account anyways, so it should be fine.
+
+# License
+
+I am releasing it under the MIT license which means you are welcome to use it for any and all purposes as long as you do not hold me liable. License details are below. You may contact me if you need it released under a different license.
+
+MIT License
+
+Copyright (c) 2016 Brent Nesbit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ yealink-directory-creator.php
@@ -0,0 +1,213 @@
+<?php
+
+###################################################
+// Header: Set our variables/constants/etc
+###################################################
+
+date_default_timezone_set('America/New_York');				// Not required, but usually a good idea in php
+
+define("SERVER", "nms.example.com");						// Base URL to your nms server. No https or trailing slashes.
+define("SUPERUSER", "directorycreator@example.com"); 		// A superuser login. Should use "Super User Read Only" for security.
+define("PASSWORD", "Strong-Password-Here");					// Password for the above account
+define("CLIENTID", "Example_API_User");						// API key client ID
+define("CLIENTSECRET", "ExampleKey123");					// API key secret key
+
+define("DIRECTORYLOCATION", "/var/www/html/example/");		// Absolute path to folder where directories will be stored. INCLUDE TRAILING SLASH.
+
+# $startTime = __pageLoadTimer();				// Track script load/processing time. See comments in the function definition at the bottom of this file.
+												// Must uncomment here and in footer at the bottom to use it.
+
+###################################################
+// Step 1: Get API access token from the NMS
+###################################################
+
+$query = array(
+    'grant_type' => "password",
+    'username' => SUPERUSER,
+    'password' => PASSWORD,
+    'client_id' => CLIENTID,
+    'client_secret' => CLIENTSECRET
+);
+
+$postFields = http_build_query($query);
+
+$curl_result = __doCurl("https://" . SERVER . "/ns-api/oauth2/token", CURLOPT_POST, NULL, NULL, $postFields, $http_response);
+
+if (!$curl_result) {					// Check if curl result was unsuccessful. Check 1 of 3. 
+	# echo "Server error";				// Uncomment for basic debugging
+	exit;								// Curl failed. Exiting so we don't overwrite all of our directories with failed results.
+}
+
+if ($http_response != "200") {											// Check if we got something other than 200/OK on key request. Check 2 of 3. 
+	# echo "Key status: FAIL. http_response: $http_response.<br>";		// Uncomment for basic debugging
+	exit;																// Key retrieval failed. Exiting so we don't overwrite all of our directories with failed results.
+}
+else {
+	# echo "Key status: PASS<br>";					// Uncomment for basic debugging
+}
+
+$token = json_decode($curl_result, true);			// Decode JSON response
+
+if (!isset($token['access_token'])) {				// Verify we got a token
+    # echo "No token received.";					// Uncomment for basic debugging
+    exit;											// Key retrieval failed. Exiting so we don't overwrite all of our directories with failed results. Check 3 of 3. 
+}
+
+$token = $token['access_token'];					// Set our API token as $token
+# echo "<br>Token: $token";							// Uncomment for basic debugging
+
+
+###################################################
+// Step 2: Retrieve list of domains
+###################################################
+
+$query = array(
+    'object' => "domain",			// Request for list of all domains
+    'action' => "read"
+);
+
+$domains  = __doCurl("https://" . SERVER . "/ns-api/", CURLOPT_POST, "Authorization: Bearer " . $token, $query, null, $http_response);
+
+$domains = simplexml_load_string($domains);					// Load the XML response from the API
+
+foreach($domains->domain as $key => $value) {  				// Look for the "domain" field in the API response
+	if(isset($value->domain)){  							// If the "domain" field has a value then:
+		$domainArray[] = "$value->domain";  				// Build an array with the domains
+	}
+}
+
+###################################################
+// Step 3a: Begin creating directories
+###################################################
+
+foreach ($domainArray as $key => $domain) {								// Loop through each item (domain) in the array, one at a time.
+	# echo "Domain: $value.<br>";										// Uncomment for basic debugging
+
+	set_time_limit(30);  												// Prevent PHP timeout while building the directories. Sets it to 30 seconds at the beginning of each loop.
+
+	$userList = "";     												// Blank out our user list at the start of each loop
+
+	$userList .= '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n";    // This is the XML header, required by Yealink for the directory
+	$userList .= '<YealinkIPPhoneDirectory>'."\n";
+
+###################################################
+// Step 3b: Retrieve list of users (subscribers)
+###################################################
+
+	$query = array(
+    'object' => "subscriber",											// Retrieve a full list of all subscribers for this domain
+    'action' => "read",
+	'domain' => htmlspecialchars($domain),								// Sanitizing the domain name, just to be safe
+    'fields' => "user,srv_code,dir_list,first_name,last_name"			// We only need specific elements from the subscriber list
+	);
+
+	$subscribers = __doCurl("https://" . SERVER . "/ns-api/", CURLOPT_POST, "Authorization: Bearer " . $token, $query, null, $http_response);
+
+	$subscribers = simplexml_load_string($subscribers);   								// Load the XML response from the API
+
+	foreach($subscribers->subscriber as $key => $value) {								// Take each subscriber (user) found for this domain, one subscriber at a time
+			if(isset($value->srv_code) && $value->srv_code == 'active-user'){			// *!* SEE NOTE IN README *!* Check to see if they are set as an "active-user" -- Don't add them if they aren't. 
+				if($value->dir_list == 'yes'){											// Check to see if they are shown in the portal directory -- Don't add them if they are hidden.
+					$userList .= '<DirectoryEntry>'."\n";													// More Yealink XML
+					$userList .= '<Name>' . $value->first_name . " " . $value->last_name ."</Name> \n";		// Add first name and last name to the directory
+					$userList .= '<Telephone>' . $value->user . "</Telephone> \n";							// Add the telephone number to the directory
+					$userList .= '</DirectoryEntry>'."\n";
+				}
+			}
+		}
+	$userList .= '</YealinkIPPhoneDirectory>'."\n";    									// Yealink XML footer
+
+###################################################
+// Step 4: Save directory to a file
+###################################################
+
+	$saveDirectory = fopen(DIRECTORYLOCATION . $domain . ".xml", "w");							// Open location to save to. It always overwrites the whole file.
+	
+	if (fwrite($saveDirectory, $userList) === FALSE) {											// Write the file, and check if failed to save
+		# echo "Failed to write to file: " . DIRECTORYLOCATION . $domain . ".xml <br>";			// Uncomment for basic debugging
+	}
+	else {
+		# echo "Successfully wrote: " . DIRECTORYLOCATION . $domain . ".xml <br>";				// Uncomment for basic debugging
+	}
+	fclose($saveDirectory);																		// Close the file
+
+}
+
+###################################################
+// Footer
+###################################################
+
+# echo "Directories generated in " . __pageLoadTimer($startTime) . " seconds.";					// Uncomment to use load timer (2 of 2)
+
+
+###################################################
+// Functions stored below 
+###################################################
+
+function __pageLoadTimer($startTime = NULL){				// Track load/processing time. Can be used for debugging purposes, or for adding additional checks to verify if the script
+															// may have failed (ex: if it runs too quickly/too slowly compared to your baseline). Must uncomment in Header and Footer to use it.
+
+		$time = microtime();								// Initialize microtime function
+		$time = explode(' ', $time);
+		$time = $time[1] + $time[0];
+
+	if ($startTime == NULL) {								// We don't have a start time yet, so set one
+		$startTime = $time;
+
+		return $startTime;
+	}
+
+	else {													// We have a start time. Calculate finish time and total time
+		$finish = $time;
+		$total_time = round(($finish - $startTime), 4);
+
+		return $total_time;
+	}
+
+}
+
+
+function __doCurl($url, $method, $authorization, $query, $postFields, &$http_response) {		// Function for our curl requests. Taken from aaker's github. Source: https://github.com/aaker/domain-selfsignup
+	$start        = microtime(true);
+	$curl_options = array(
+		CURLOPT_URL => $url . ($query ? '?' . http_build_query($query) : ''),
+		CURLOPT_SSL_VERIFYHOST => false,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FORBID_REUSE => true,
+		CURLOPT_TIMEOUT => 60
+	);
+
+	$headers = array();
+	if ($authorization != NULL)
+		{
+		$headers[$authorization] = $authorization;
+		} //$authorization != NULL
+
+
+
+	$curl_options[$method] = true;
+	if ($postFields != NULL)
+		{
+		$curl_options[CURLOPT_POSTFIELDS] = $postFields;
+		} //$postFields != NULL
+
+	if (sizeof($headers) > 0)
+		$curl_options[CURLOPT_HTTPHEADER] = $headers;
+
+	$curl_handle = curl_init();
+	curl_setopt_array($curl_handle, $curl_options);
+	$curl_result   = curl_exec($curl_handle);
+	$http_response = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+	//print_r($http_response);
+	curl_close($curl_handle);
+	$end = microtime(true);
+	if (!$curl_result)
+		return NULL;
+	else if ($http_response >= 400)
+		return NULL;
+	else
+		return $curl_result;
+	}
+
+?>
